209b488d86a7adac2abdb30d206679c9bee2c2b8
NItesh Shrestha <shrnitt88@gmail.com>
2025-03-07 20:07:43 +0545

    Initial commmit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import Search from './component/search'
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('')
+
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without a Hassle
+          </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+      </div>
+    </main>
+  )
+}
+
+export default App
